122c9e92c836b8e365f91bc869d8804d90ceabd8
paulrbts <paulrbts@users.noreply.github.com>
2017-08-10 09:19:48 +0100

    added aliases, sorted aliases alphabetically

--- aliases.zsh
+++ aliases.zsh
@@ -1,11 +1,6 @@
 # Homebrew
 alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
 
-# Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
-alias weather="curl http://wttr.in/Stafford"
-
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
